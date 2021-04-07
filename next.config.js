026601bdfadbd18deba84e5f22241c596c1d4d7b
Will Strasser <wstrasser@hioscar.com>
2021-04-07 19:13:02 -0400

    upgrade next and add redirect from /mob -> /

--- next.config.js
+++ next.config.js
@@ -2,4 +2,14 @@ module.exports = {
   env: {
     API_TOKEN: process.env.BUTTER_API_TOKEN,
   },
+  async redirects() {
+    console.log('helllllooo');
+    return [
+      {
+        source: '/mob',
+        destination: '/',
+        permanent: true,
+      },
+    ];
+  },
 };
