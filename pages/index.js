1154a90f86f4b717b762419f2491d68df49ff541
Will Strasser <will.strasser@gmail.com>
2024-04-23 14:25:06 -0700

    separate mobile

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
-import Image from "next/legacy/image";
+import Image from 'next/legacy/image';
 
 import SwiperCore, {Autoplay, EffectFade} from 'swiper';
 import {Swiper, SwiperSlide} from 'swiper/react';
 
+import {useMediaQuery} from '/components/useMediaQuery';
 import Layout from 'components/Layout';
 import InstagramLink from 'components/InstagramLink';
 import {fetchPageData} from 'utils/fetchPageData';
@@ -11,8 +12,11 @@ import styles from './index.module.css';
 
 SwiperCore.use([Autoplay, EffectFade]);
 
-export default function HomePage({images, preview}) {
+export default function HomePage({images, mobileImages, preview}) {
+  const mobileFallback = mobileImages || images;
+  const isMobile = useMediaQuery(`(max-width: 767.98px)`);
   const imageOptions = process.env.NODE_ENV === 'development' ? `?w=${500}` : '';
+
   return (
     <>
       <Layout preview={preview}>
@@ -25,17 +29,30 @@ export default function HomePage({images, preview}) {
             effect="fade"
             spaceBetween={0}
           >
-            {images.map((image) => (
-              <SwiperSlide key={image.sys.id}>
-                <div style={{height: '100vh', width: '100vw'}}>
-                  <Image
-                    alt={image.image_description}
-                    src={`https:${image.fields.file.url}${imageOptions}`}
-                    layout="fill"
-                  />
-                </div>
-              </SwiperSlide>
-            ))}
+            {(!isMobile || isMobile === undefined) &&
+              images.map((image) => (
+                <SwiperSlide key={image.sys.id}>
+                  <div style={{height: '100vh', width: '100vw'}}>
+                    <Image
+                      alt={image.image_description}
+                      src={`https:${image.fields.file.url}${imageOptions}`}
+                      layout="fill"
+                    />
+                  </div>
+                </SwiperSlide>
+              ))}
+            {(isMobile || isMobile === undefined) &&
+              mobileFallback.map((image) => (
+                <SwiperSlide key={image.sys.id}>
+                  <div style={{height: '100vh', width: '100vw'}}>
+                    <Image
+                      alt={image.image_description}
+                      src={`https:${image.fields.file.url}${imageOptions}`}
+                      layout="fill"
+                    />
+                  </div>
+                </SwiperSlide>
+              ))}
           </Swiper>
         </div>
         <InstagramLink isAbsolute={true} />
