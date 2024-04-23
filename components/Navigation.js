25378d5c311609665ac789289a5e9eced545e914
Will Strasser <will.strasser@gmail.com>
2024-04-23 12:34:22 -0700

    logo fix

--- components/Navigation.js
+++ components/Navigation.js
@@ -7,8 +7,8 @@ import styles from './Navigation.module.css';
 
 let pages = [
   {slug: '', title: 'Campbell & Strasser', inSplash: true},
-  {slug: 'the-shop', title: 'The Shop', inSplash: false},
-  {slug: 'gallery', title: 'Projects', inSplash: false},
+  // {slug: 'the-shop', title: 'The Shop', inSplash: false},
+  // {slug: 'gallery', title: 'Projects', inSplash: false},
   {slug: 'contact', title: 'Contact', inSplash: true},
 ];
 function Navigation({preview}) {
@@ -37,30 +37,32 @@ function Navigation({preview}) {
     });
   }
   const router = useRouter();
-  return <>
-    <ul className={styles.nav}>
-      <li className={classNames(styles.navItem, styles.logoItem)} key={pages[0].slug}>
-        <Link
-          className={classNames(styles.navLink, styles.logo)}
-          href={`/${pages[0].slug}`}
-          legacyBehavior>
-          {pages[0].title}
-        </Link>
-      </li>
-      {renderLinks()}
-    </ul>
-    <ul className={classNames(styles.mobileNav, {[styles.open]: open})}>
-      {renderLinks()}
-    </ul>
-    <div
-      onClick={() => setOpen(!open)}
-      className={classNames(styles.navToggle, {[styles.active]: open})}
-    >
-      <i></i>
-      <i></i>
-      <i></i>
-    </div>
-  </>;
+  return (
+    <>
+      <ul className={styles.nav}>
+        <li className={classNames(styles.navItem, styles.logoItem)} key={pages[0].slug}>
+          <Link
+            className={classNames(styles.navLink, styles.logo)}
+            href={`/${pages[0].slug}`}
+          >
+            {pages[0].title}
+          </Link>
+        </li>
+        {renderLinks()}
+      </ul>
+      <ul className={classNames(styles.mobileNav, {[styles.open]: open})}>
+        {renderLinks()}
+      </ul>
+      <div
+        onClick={() => setOpen(!open)}
+        className={classNames(styles.navToggle, {[styles.active]: open})}
+      >
+        <i></i>
+        <i></i>
+        <i></i>
+      </div>
+    </>
+  );
 }
 
 export default Navigation;
